ce2ff4b4a45e34e0bf80eb7430b88b7395c4eb37
scherepa <scherepa@gmail.com>
2021-06-06 19:08:06 +0300

    make it better

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/",(req,res) => {
-  res.json({msg:"welcome to my api"})
+  res.json({msg:"welcome to my bizCards project!"})
 })
 
 module.exports = router;
