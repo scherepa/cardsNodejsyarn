4e5a6987e813aeacf4266e00500cc0de9838169a
scherepa <scherepa@gmail.com>
2021-06-08 10:42:14 +0300

    message

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/",(req,res) => {
-  res.json({msg:"welcome to my bizCards project!"})
+  res.json({msg:"welcome to my bizCards project for heroku!"})
 })
 
 module.exports = router;
